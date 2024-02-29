13d4e555322e0e345c6774dd1e8d26cb02a083e2
Kajal Naik <naikkajal0603@gmail.com>
2024-02-29 20:21:12 +0530

    how to intialize an array

--- src/Arrays.java
+++ src/Arrays.java
@@ -2,13 +2,12 @@ import java.util.*;
 
 public class Arrays {
     public static void main(String[] args) {
-        int[] marks=new int[4];
-        marks[0]=97;
-        marks[1]=65;
-        marks[2]=88;
-        marks[3]=79;
-        for(int i=0;i<4;i++){
-            System.out.println(marks[1]);
+        Scanner sc= new Scanner(System.in);
+        int size= sc.nextInt();
+        int numbers[]=new int[size];
+
+        for(int i=0;i<size;i++){
+            System.out.println(numbers[i]);
         }
     }
 }
